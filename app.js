a0e60e03264a115d702e9abcfb0f5a0ffc8e077f
Tonmoy Mandal <tonmoymandal8@gmail.com>
2024-10-20 14:39:52 +0530

    modified app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL,"http://localhost:5173" ],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173", "https://react-to-do-app-five.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
